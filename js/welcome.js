b43eb8825397d9032d3c256c036a1f9c2fdc104f
jwhester <jameshester88@gmail.com>
2019-09-19 21:52:13 -0500

    changed everything style wise on the welcome page

---
+++ js/welcome.js
@@ -0,0 +1,45 @@
+function showDiv0() {
+    var x = document.getElementById("coreValues");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function showDiv1() {
+    var x = document.getElementById("ourProcess");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function showDiv2() {
+    var x = document.getElementById("vision");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function showDiv3() {
+    var x = document.getElementById("companyVision");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function showDiv4() {
+    var x = document.getElementById("ourProduct");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
