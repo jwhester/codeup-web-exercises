6120df0dccef00aefd559f17005341f451b2e799
jwhester <jameshester88@gmail.com>
2019-10-09 09:29:27 -0500

    9-OCT-19

---
+++ js/practice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function multiplication(x, y){
+    return x * y;
+}
+
+// var multiplication = function (x, y) {
+//     return x * y;
+// };
+
+function division(x, y) {
+    return x / y;
+}
