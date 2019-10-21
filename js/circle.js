b28e02e09691f866893d1999bbdb7068463e655b
jwhester <jameshester88@gmail.com>
2019-10-21 10:42:12 -0500

    finished

--- js/circle.js
+++ js/circle.js
@@ -1,30 +1,29 @@
-(function() {
+
     "use strict";
 
     // create a circle object
     var circle = {
         radius: 3,
 
-        getArea: function (element) {
+        getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
-
-            return Math.PI *  Math.pow(element.radius, 2); // TODO: return the proper value
+            var area = Math.PI *  Math.pow(this.radius, 2);
+            // TODO: return the proper value
+            return area;
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-            if (doRounding === Math.round(doRounding)){
-                return Math.round(doRounding);
+            if (doRounding){
+                return Math.round(this.getArea());
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
-            }else{
-                return doRounding;
             }
-            console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea(doRounding));
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
         }
     };
-
+    console.log(circle.logInfo(true));
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
@@ -33,10 +32,9 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+        circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
     circle.logInfo(true);
-})();
