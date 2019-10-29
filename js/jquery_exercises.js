4b3c2d72633fc5f2a0570dc958e75abfa918025e
jwhester <jameshester88@gmail.com>
2019-10-29 13:54:25 -0500

    finished

---
+++ js/jquery_exercises.js
@@ -0,0 +1,33 @@
+"use strict";
+
+$(document).ready(function () {
+    //Alerts the h1 elements on page load.
+    var heading = $('h1').html();
+    $(document).ready(function () {
+        alert(heading);
+    });
+    //changes color of body
+    $('body').css('background-color', 'black');
+    $('.codeup').css('border', '1px solid red');
+
+    //Changes fonts of list items to 20px.
+    $('li').css('font-size', '20px');
+
+    //Highlights h1, p, and li tags to orange.
+    $('h1, p, li').css('background-color', 'orange');
+
+    //heading font size
+    $('#heading').css({'font-size': '50px', 'margin-top': '10%' });
+
+    //p tag font
+    $('p').css('font-size', '20px');
+
+    //Collapsible tabs
+    $( function() {
+        $( "#tabs" ).tabs();
+    } );
+
+
+    //centers header
+    $('#head').css('margin-left','15%')
+});
